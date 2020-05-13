b332bc53528701c6fcde3e6f6c4a475350eea78a
szymon <szymon.kuc@poczta.onet.pl>
2020-05-13 19:57:15 +0200

    counting time in all websites + saving in localstorage

--- background.js
+++ background.js
@@ -1,54 +1,88 @@
 console.log("background running!");
 
-var localStorage = localStorage.getItem("time");
-let time_local = {
-    seconds: JSON.parse(localStorage.time).seconds == undefined ? 0 : JSON.parse(localStorage.time).seconds,
-    minutes: JSON.parse(localStorage.time).minutes == undefined ? 0 : JSON.parse(localStorage.time).minutes,
-    hours: JSON.parse(localStorage.time).hours == undefined ? 0 : JSON.parse(localStorage.time).hours
-}
-window.time = localStorage === null ? 0 : (time_local.hours < 10 ? ("0"+time_local.hours) : time_local.hours) + ":" + (time_local.minutes < 10 ? ("0"+time_local.minutes) : time_local.minutes) + ":" + (time_local.seconds < 10 ? ("0"+time_local.seconds) : time_local.seconds);
-let interval;
-let counter = () => {
-    time_local.seconds++;
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.text == "what is my tab_id?") {
+        sendResponse(sender.tab);
+    }
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if(message.closeThis) chrome.tabs.remove(sender.tab.id);
+  });
+
+// let isActive = false;
+// var localStorage = localStorage.getItem("time")
+
+
+// let interval;
+// let time_local = {
+//     seconds: 0,
+//     minutes: 0,
+//     hours: 0
+// }
+// timer();
+// let tab_id = 0;
+
+// let counter = () => {
+//     time_local.seconds++;
     
-    if(time_local.seconds > 59){
-        time_local.minutes++;
-        time_local.seconds = 0;
-        localStorage.setItem('time', JSON.stringify(time_local));
-    }
-    if(time_local.minutes > 59){
-        time_local.hours++;
-        time_local.minutes = 0;
-    }
-    window.time = (time_local.hours < 10 ? ("0"+time_local.hours) : time_local.hours) + ":" + (time_local.minutes < 10 ? ("0"+time_local.minutes) : time_local.minutes) + ":" + (time_local.seconds < 10 ? ("0"+time_local.seconds) : time_local.seconds);
-    console.log(time_local.seconds);
-}
+//     if(time_local.seconds > 59){
+//         time_local.minutes++;
+//         time_local.seconds = 0;
+//         localStorage.setItem('time', JSON.stringify(time_local));
+//     }
+//     if(time_local.minutes > 59){
+//         chrome.tabs.remove(tab_id);
+//     }
+//     if(time_local.minutes > 59){
+//         time_local.hours++;
+//         time_local.minutes = 0;
+//     }
+//     window.time = (time_local.hours < 10 ? ("0"+time_local.hours) : time_local.hours) + ":" + (time_local.minutes < 10 ? ("0"+time_local.minutes) : time_local.minutes) + ":" + (time_local.seconds < 10 ? ("0"+time_local.seconds) : time_local.seconds);
+//     console.log(time_local.seconds);
+//     isActive = true;
+// }
 
-chrome.tabs.onActivated.addListener(getUrl);;
-
-function getUrl() {
-    chrome.tabs.query(
-        {
-         lastFocusedWindow: true,
-         active: true
-        },
-        function (tabs)
-        {
-            let url = tabs[0].url;
-
-            if(url.includes("youtube")){
-                interval = setInterval(counter, 1000)
-            }
-            else{
-                 clearInterval(interval);
-            }
-        });
-}
-getUrl();
-chrome.tabs.onRemoved.addListener(function(tabid, removed) {
-        localStorage.setItem('time', JSON.stringify(time_local));
-   })
+// chrome.tabs.onActivated.addListener(getUrl);
+// chrome.tabs.onUpdated.addListener(getUrl);
+// function getUrl() {
+//     chrome.tabs.query({},
+//         function (tabs)
+//         {
+//             for(i=0;i<=tabs.length; i++){
+//                 console.log(tabs[i]);
+//                 if(tabs[i].url.includes("youtube") || tabs[i].url.includes("facebook") && isActive==false){
+//                     interval = setInterval(counter, 1000);
+//                     isActive = true;
+//                     tab_id = tabs[i].id;
+//                     break;
+//                 }
+//                 else{
+//                     clearInterval(interval);
+//                     isActive = false;
+//                }
+//             }
+//         });
+// }
+// getUrl();
+// chrome.tabs.onRemoved.addListener(function(tabid, removed) {
+//         getUrl();
+//         localStorage.setItem('time', JSON.stringify(time_local));
+//    })
    
+
+// function timer(){
+
+//     if(localStorage.length != 0){
+//         time_local = {
+//             seconds: JSON.parse(localStorage.time).seconds == undefined ? 0 : JSON.parse(localStorage.time).seconds,
+//             minutes: JSON.parse(localStorage.time).minutes == undefined ? 0 : JSON.parse(localStorage.time).minutes,
+//             hours: JSON.parse(localStorage.time).hours == undefined ? 0 : JSON.parse(localStorage.time).hours
+//         }
+//     };
+    
+//     window.time = localStorage === null ? 0 : (time_local.hours < 10 ? ("0"+time_local.hours) : time_local.hours) + ":" + (time_local.minutes < 10 ? ("0"+time_local.minutes) : time_local.minutes) + ":" + (time_local.seconds < 10 ? ("0"+time_local.seconds) : time_local.seconds);
+// }
 
 
 
--- content.js
+++ content.js
@@ -1,8 +1,85 @@
 console.log("Chrome extension go");
+let word ;
+let ls;
+let tab_id = 0;
+chrome.runtime.sendMessage({ text: "what is my tab_id?" }, tab => {
+    let url = tab.url;
+    tab_id = tab.id;
+    let index;
+    let index2;
+    if(tab.url.includes("www")){
+        index = url.indexOf(".") + 1;
+        index2 = url.indexOf(".", index);
+    }
+    else {
+        index = url.indexOf("/") + 2;
+        index2 = url.indexOf(".");
+    }
+    word = url.slice(index, index2);
+    ls =  JSON.parse(localStorage.getItem(word));
+    timer();
+ });
 
-chrome.runtime.onMessage.addListener(gotMessage);
+let time_local = {
+    seconds: 0,
+    minutes: 0,
+    hours: 0
+}
+function timer(){
+    if(localStorage.length != 0){
+        time_local = {
+            seconds: ls.seconds == undefined ? 0 : ls.seconds,
+            minutes: ls.minutes == undefined ? 0 : ls.minutes,
+            hours: ls.hours == undefined ? 0 : ls.hours
+        }
+    };
 
-function gotMessage(message, sender, sendResponse){
-    console.log(message);
+}
+let counter = () => {
+    time_local.seconds++;
+
+    if(time_local.seconds > 59){
+        time_local.minutes++;
+        time_local.seconds = 0;
+        localStorage.setItem(word, JSON.stringify(time_local));
+    }
+
+    if(time_local.minutes > 1 && word=="youtube"){
+        chrome.runtime.sendMessage({closeThis: true});
+    }
+    if(time_local.minutes > 59){
+        time_local.hours++;
+        time_local.minutes = 0;
+    }
+    console.log(time_local.minutes +":" + time_local.seconds);
 }
 
+setInterval(counter, 1000);
+
+// else{
+//     clearInterval(interval);
+//     isActive = false;
+// }
+
+// chrome.tabs.onActivated.addListener(getUrl);
+// chrome.tabs.onUpdated.addListener(getUrl);
+// function getUrl() {
+//     chrome.tabs.query({},
+//         function (tabs)
+//         {
+//             for(i=0;i<=tabs.length; i++){
+//                 console.log(tabs[i]);
+//                 if(tabs[i].url.includes("youtube") || tabs[i].url.includes("facebook") && isActive==false){
+//                     interval = setInterval(counter, 1000);
+//                     isActive = true;
+//                     tab_id = tabs[i].id;
+//                     break;
+//                 }
+//                 else{
+//                     clearInterval(interval);
+//                     isActive = false;
+//                }
+//             }
+//         });
+// }
+// getUrl();
